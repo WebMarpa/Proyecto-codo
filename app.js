f6f0f5dbc097e1e75b5acfc6f4c31cf3e92b6f13
WebMarpa <mario_amilcar_parodi@hotmail.com>
2023-09-30 13:44:04 -0300

    modificación barra responsive

--- app.js
+++ app.js
@@ -212,3 +212,12 @@ inputs.forEach((input) => {
 
 })
 
+// función boton menu responsive
+function responsiveMenu() {
+  var x = document.getElementById('nav');
+  if (x.className ==="") {
+    x.className = "responsive";
+  } else{
+    x.className="";
+  }
+}

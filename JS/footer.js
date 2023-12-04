9c0113f5ea232d87d82d2f27406ddc6ae44d2cbb
WebMarpa <mario_amilcar_parodi@hotmail.com>
2023-12-03 22:00:01 -0300

    Cambios Panel de control y tabla agregado footer

---
+++ JS/footer.js
@@ -0,0 +1,3 @@
+document.getElementById("foo").innerHTML=` <div class="copyright-texto">
+<p>&#169 2023 MARPA Laser. Todos los derechos reservados.</p>
+</div> `
--- JS/main.js
+++ JS/main.js
@@ -32,4 +32,7 @@ document.getElementById("header").innerHTML=` <div id="menu-nav">
     </div>
 </div>
 </div>
-`
+`,
+document.getElementById("foo").innerHTML=` <div class="copyright-texto">
+<p>&#169 2023 MARPA Laser. Todos los derechos reservados.</p>
+</div> `;
